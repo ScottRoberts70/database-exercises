c37885dd0a1d172a38edd59c0219e2496ee44420
Scott Roberts <scott.w.roberts70@gmail.com>
2022-07-06 11:53:42 -0500

    Second commit for SQL

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50) NOT NULL,
-                        name  VARCHAR(50) NOT NULL,
+                        name  VARCHAR(100) NOT NULL,
                         release_date INT NOT NULL,
                         sales INT UNSIGNED NOT NULL,
                         genre VARCHAR(50) NOT NULL,
